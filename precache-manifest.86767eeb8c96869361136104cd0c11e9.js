be4c4bd677665e5c72e226975741b1d729ece613
p1xan <pixan888@gmail.com>
2020-09-28 20:22:08 +0300

    Updates

---
+++ precache-manifest.86767eeb8c96869361136104cd0c11e9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30c6834e5ac3a7d5b5800f63e70e1503",
+    "url": "/todolist/index.html"
+  },
+  {
+    "revision": "60583df270db64361628",
+    "url": "/todolist/static/css/main.a3c5243e.chunk.css"
+  },
+  {
+    "revision": "450f276f8698fefa4ecc",
+    "url": "/todolist/static/js/2.2599a007.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todolist/static/js/2.2599a007.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "60583df270db64361628",
+    "url": "/todolist/static/js/main.c8634533.chunk.js"
+  },
+  {
+    "revision": "471e54e937a3aef18e16",
+    "url": "/todolist/static/js/runtime-main.283d8678.js"
+  }
+]);
